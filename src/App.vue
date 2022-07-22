bb9c3fa86d5f6411593c9ba85d4ef4120a893eda
Albeiro Lozano <57449621+Albe036@users.noreply.github.com>
2022-07-22 08:26:35 -0500

    created branch sign_in_user

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/homePage.vue';
 </script>
 
 <template>
   <div>
+    <HomePage></HomePage>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
